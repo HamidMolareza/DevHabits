e621a82f4fc06ddf37cb8ab3355fd3c015c412be
HamidMolareza <molareza@protonmail.com>
2025-08-24 07:50:37 +0330

    chore: minor updates

--- DevHabits.Api/Controllers/WeatherForecastController.cs
+++ DevHabits.Api/Controllers/WeatherForecastController.cs
@@ -4,16 +4,12 @@ namespace DevHabits.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase {
-    private static readonly string[] Summaries = new[] {
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase {
+    private static readonly string[] Summaries = [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    ];
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger) {
-        _logger = logger;
-    }
+    private readonly ILogger<WeatherForecastController> _logger = logger;
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get() {
