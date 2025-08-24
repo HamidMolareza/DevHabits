d71f2f4cb949ca5c9061eea7165eb83a609a0ccc
HamidMolareza <molareza@protonmail.com>
2025-08-24 19:23:26 +0330

    chore: add OpenTelemetry

--- DevHabits.Api/Program.cs
+++ DevHabits.Api/Program.cs
@@ -1,3 +1,8 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,27 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Configure OpenTelemetry for distributed tracing and metrics
+builder.Services.AddOpenTelemetry()
+    // Set resource information (service name)
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    // Enable tracing with HTTP client and ASP.NET Core instrumentation
+    .WithTracing(tracing => tracing
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation())
+    // Enable metrics with HTTP client, ASP.NET Core, and runtime instrumentation
+    .WithMetrics(metrics => metrics
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation())
+    // Export telemetry data using OTLP protocol
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options => {
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
 
 WebApplication app = builder.Build();
 
